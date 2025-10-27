4c0426d1330444ec8e256c22485b82d3d6255ae2
Switer <hungtranpc1342l6@gmail.com>
2025-10-27 12:00:25 +0700

    Push ITS

---
+++ DataSet/object detection.v1i.yolov11/README.dataset.txt
@@ -0,0 +1,6 @@
+# object detection > 2023-09-14 4:53pm
+https://universe.roboflow.com/ctu-ccvhy/object-detection-nbcht
+
+Provided by a Roboflow user
+License: CC BY 4.0
+
--- train_yolov11.py
+++ train_yolov11.py
@@ -4,29 +4,30 @@ import multiprocessing
 def main():
     multiprocessing.freeze_support()
 
-    model = YOLO(r"runs/detect/train_v8_plate_pro2/weights/best.pt")
+    model = YOLO(r"Yolo-Weights/yolo11n.pt")
 
     model.train(
-        data=r"LP detection.v1i.yolov8\data.yaml",
-        epochs=150,
-        imgsz=960,
-        batch=16,
-        lr0=8e-4,
+        data=r"DataSet/object detection.v1i.yolov11/data.yaml",
+        epochs=80,
+        imgsz=640,
+        batch=4,
+        lr0=0.001,
         optimizer="AdamW",
-        mosaic=1.0,
-        mixup=0.3,
-        translate=0.2,
-        scale=0.5,
+        mosaic=0.8,
+        mixup=0.2,
+        translate=0.1,
+        scale=0.3,
         degrees=10,
-        shear=0.15,
-        hsv_h=0.03,
-        hsv_s=0.8,
-        hsv_v=0.6,
-        patience=80,
+        shear=0.1,
+        hsv_h=0.015,
+        hsv_s=0.7,
+        hsv_v=0.5,
+        patience=40,
         device=0,
-        name="train_v8_plate_5",
+        name="License_Plate_Models",
         pretrained=True,
         deterministic=True,
+        workers=2,
     )
 
 if __name__ == "__main__":
